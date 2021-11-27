a8f86c0888805320abd6628e5b36001ca8b3be6a
Martin <martin@cardiologs.com>
2021-11-27 21:55:16 +0100

    init

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+"phantombuster package: 6";
+"phantombuster command: nodejs";
+"phantombuster flags: save-folder";
+
+(async () => {
+  console.log("Hello from the other side!");
+  process.exit();
+})();
