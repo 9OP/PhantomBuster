2569fe3c10c2b0d4a807d15d92f711c230b353d4
Martin <martin@cardiologs.com>
2021-11-28 10:29:35 +0100

    🔥  Ajv validation

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@
 
 import Buster from "phantombuster";
 import puppeteer from "puppeteer";
+import Ajv from "ajv";
 
 const MARMITON_URL = "https://www.marmiton.org";
 
@@ -15,38 +16,92 @@ const SELECTORS: Record<keyof IRecipe, string> = {
   reviews: ".MRTN__sc-30rwkm-3",
 };
 
+const _filterType = [
+  "entree",
+  "platprincipal",
+  "dessert",
+  "amusegueule",
+  "sauce",
+  "accompagnement",
+  "boisson",
+  "confiserie",
+] as const;
+const _filterDifficulty = [1, 2, 3, 4] as const;
+const _filterExpense = [1, 2, 3] as const;
+const _filterParticularity = [1, 2, 3, 4, 5] as const;
+const _filterCooking = [1, 2, 3, 4, 5] as const;
+const _filterTime = [15, 30, 45] as const;
+
+const ARGS_SCHEMA = {
+  title: "Args",
+  description: "Marmiton search args",
+  type: "object",
+  properties: {
+    search: { type: "string" },
+    type: {
+      type: "array",
+      items: {
+        type: "string",
+        enum: _filterType,
+      },
+    },
+    difficulty: {
+      type: "array",
+      items: {
+        type: "number",
+        enum: _filterDifficulty,
+      },
+    },
+    expense: {
+      type: "array",
+      items: {
+        type: "number",
+        enum: _filterExpense,
+      },
+    },
+    particularity: {
+      type: "array",
+      items: {
+        type: "number",
+        enum: _filterParticularity,
+      },
+    },
+    cooking: {
+      type: "array",
+      items: {
+        type: "number",
+        enum: _filterCooking,
+      },
+    },
+    time: {
+      type: "array",
+      items: {
+        type: "number",
+        enum: _filterTime,
+      },
+    },
+  },
+  required: ["search"],
+};
+
 // Represents a scrapped recipe from marmiton website
 interface IRecipe {
   name: string;
   url: string;
-  rating: string;
-  reviews: string;
+  rating: number;
+  reviews: number;
 }
 
 // Search parameters
 interface IFilter {
   search: string; // aqt
-  type?: IFilterType[]; // dt
-  difficulty?: IFilterDifficulty[]; // dif
-  expense?: IFilterExpense[]; // exp
-  particularity?: IFilterParticularity[]; // prt
-  cooking?: IFilterCooking[]; // rct
-  time?: IFilterTime[]; // ttlt
+  type?: typeof _filterType[number][]; // dt
+  difficulty?: typeof _filterDifficulty[number][]; // dif
+  expense?: typeof _filterExpense[number][]; // exp
+  particularity?: typeof _filterParticularity[number][]; // prt
+  cooking?: typeof _filterCooking[number][]; // rct
+  time?: typeof _filterTime[number][]; // ttlt
 }
-type IFilterType =
-  | "entree"
-  | "platprincipal"
-  | "dessert"
-  | "amusegueule"
-  | "sauce"
-  | "accompagnement"
-  | "boisson"
-  | "confiserie";
-type IFilterDifficulty = 1 | 2 | 3 | 4;
-type IFilterExpense = 1 | 2 | 3;
-type IFilterParticularity = 1 | 2 | 3 | 4 | 5;
-type IFilterCooking = 1 | 2 | 3 | 4 | 5;
-type IFilterTime = 15 | 30 | 45;
 
 // Convert a param to the query parameters accepted by marmiton
 const get_query_param = (param: keyof IFilter) => {
@@ -91,6 +146,7 @@ const get_url = (search: IFilter): string => {
   return `${MARMITON_URL}/recettes/recherche.aspx?${query}`;
 };
 
+// add pages
 const scrap = async (url: string): Promise<IRecipe[]> => {
   const browser = await puppeteer.launch({
     // This is needed to run Puppeteer in a Phantombuster container
@@ -112,8 +168,8 @@ const scrap = async (url: string): Promise<IRecipe[]> => {
         data.push({
           name,
           url: marmiton_url + recipe_url,
-          rating,
-          reviews,
+          rating: Number(rating.replace(/[^0-9.]/g, "").slice(0, -1)),
+          reviews: Number(reviews.replace(/\D/g, "")),
         });
       });
       return data;
@@ -126,21 +182,47 @@ const scrap = async (url: string): Promise<IRecipe[]> => {
   return recipes;
 };
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const validate = (args: any): IFilter => {
+  const ajv = Ajv({ allErrors: true });
+  const valid = ajv.validate(ARGS_SCHEMA, args);
+
+  if (valid) {
+    return {
+      search: args["search"],
+      type: args["type"],
+      difficulty: args["difficulty"],
+      expense: args["expense"],
+      cooking: args["cooking"],
+      time: args["time"],
+    };
+  } else {
+    throw ajv.errors;
+  }
+};
+
 const main = async () => {
   // Instantiate the agent
   const agent = new Buster();
-  const args = agent.argument;
 
-  // validates argument AJV
+  // Validates args
+  const args = validate(agent.argument);
 
-  // build query
-  const url = get_url(args as IFilter);
+  // Build query
+  const url = get_url(args);
 
-  // scrap
+  // Scrap
   const recipes = await scrap(url);
 
-  console.log("arguments", args);
-  console.log("url", recipes);
+  // Save in resultObject
+  await agent.setResultObject(recipes);
+
+  // const json_url = await agent.saveText(
+  //   JSON.stringify(recipes),
+  //   "recipes.json",
+  //   "application/json"
+  // );
+  // return json_url;
 };
 
 (async () => {
